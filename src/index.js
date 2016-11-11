3e06c530a851673ac3fb3faaa74a0b627a8ef039
Jeffrey Lee <jlee@zignallabs.com>
2016-11-11 13:26:25 -0800

    Add support for Redux Chrome Extension dev tool

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(ReduxPromise)
+));
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('app')
